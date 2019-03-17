b66413dac3b6296dd35feea4c12128791356a7c7
antonio tari <antoniotari11@gmail.com>
2019-03-17 16:33:20 -0400

    added start-end dates option, added year option

--- setlist.py
+++ setlist.py
@@ -3,6 +3,10 @@ import urllib2
 import json
 import sys
 import urllib
+import datetime
+
+def remove_previous_line() :
+	sys.stdout.write("\033[F")
 
 # Print iterations progress
 def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1,length = 100, fill = '.'):
@@ -20,41 +24,57 @@ def printProgressBar (iteration, total, prefix = '', suffix = '', decimals = 1,l
     percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
     filledLength = int(length * iteration // total)
     bar = fill * filledLength + '-' * (length - filledLength)
-    sys.stdout.write("\033[F")
+    remove_previous_line()
     print("\r%s |%s| %s%% %s\r" % (prefix, bar, percent, suffix))
     # Print New Line on Complete
     if iteration == total: 
         print()
 
 OPTION_START_DATE = '-s'
+OPTION_YEAR = '-y'
 OPTION_END_DATE = '-e'
+DATE_FORMAT = '%d-%m-%Y'
 
 API_KEY = '1379c35f-1a6f-4d52-be50-24b865760a22'
 
 artist_name = ''
-start_date = ''
-end_date = ''
+start_date = datetime.datetime.strptime('1-1-1900', DATE_FORMAT)
+end_date = datetime.datetime.now()
+year = None
 
 # parsing command line arguments
 i = 1
 while i < len(sys.argv):
     arg = sys.argv[i]
-    if (arg == OPTION_START_DATE):
+    if (arg == OPTION_YEAR):
         i = i + 1
-        start_date = sys.argv[i]
+        year = sys.argv[i]
+        print "year %s ,start and end date will be used to refine the search within this year"%year
+        i = i + 1
+    elif (arg == OPTION_START_DATE):
+        i = i + 1
+        start_date_str = sys.argv[i]
+        start_date = datetime.datetime.strptime(start_date_str, DATE_FORMAT)
         print "starting date %s"%start_date
+        i = i + 1
     elif (arg == OPTION_END_DATE):
         i = i + 1
-        end_date = sys.argv[i]
+        end_date_str = sys.argv[i]
+        end_date = datetime.datetime.strptime(end_date_str, DATE_FORMAT)
         print "end date %s"%end_date
+        i = i + 1
     else:
-        artist_name = artist_name + ' ' + arg
+        artist_name = artist_name + ' ' + sys.argv[i]
 	i = i + 1
 
 artist_name = artist_name.strip()
 artist_name_enc = urllib.quote_plus(artist_name)
 url = "https://api.setlist.fm/rest/1.0/search/setlists?artistName=%s&p=%d"
-print artist_name
+# add the year to the url if passed as option
+if (year != None):
+	url = url + "&year=%s"%year
+
+print "artist name: %s\n"%artist_name
 page = 1
 
 def make_request():
@@ -80,15 +100,12 @@ page+=1
 # Initial call to print 0% progress
 printProgressBar(page, tot_pages, prefix = "Progress:", suffix = "Complete", length = 50)
 while (page<tot_pages):
-    #response_str = urllib2.urlopen(url%(artist_name_enc,page)).read()
-    #print url%(artist_name_enc,page)
-    jData = make_request() #json.loads(response_str)
-    setlists2 = jData #['setlists']
+    jData = make_request()
+    setlists2 = jData
     setlist.extend(setlists2['setlist'])
-    # printing the progress
-    #print('.')
-    printProgressBar(page, tot_pages, prefix = "Progress:", suffix = "Complete", length = 50)
     page+=1
+    printProgressBar(page, tot_pages, prefix = "Progress:", suffix = "Complete", length = 50)
+remove_previous_line()
 
 with open("data_%s.txt"%artist_name, 'w') as outfile:
     json.dump(setlist, outfile)
@@ -106,7 +123,7 @@ def parse_set_list(sets):
                     song_list.append(song['name'])
             else:
                 song_count+=1
-                song_list.append(songs['@nme'])
+                song_list.append(songs['name'])
     else:
         songs = sets['song']
         if (type(songs) is list):
@@ -116,40 +133,45 @@ def parse_set_list(sets):
         else:
             song_count+=1
             song_list.append(songs['name'])
-    print(song_count)
+
 
 for item in setlist:
-    sets1 = item['sets']
-    if (type(sets1) is dict):
-        sets = sets1['set']
-        parse_set_list(sets)
-    else:
-        for setsElem in sets1:
-            #sets = sets1['set']
-            parse_set_list(setsElem)
+	eventDate = item['eventDate']
+	eventDate_date = datetime.datetime.strptime(eventDate, DATE_FORMAT)
+	if (eventDate_date >= start_date and eventDate_date <= end_date):
+		sets1 = item['sets']
+		if (type(sets1) is dict):
+			sets = sets1['set']
+			parse_set_list(sets)
+		else:
+			for setsElem in sets1:
+				#sets = sets1['set']
+				parse_set_list(setsElem)
 
 
-file_name = "%s.txt"%artist_name
+lines = []
+file_name = "%s__%s_%s_%s.txt"%(artist_name, start_date, end_date, year)
 thefile = open(file_name, 'w')
 for item in song_list:
     line_song = ("%s\n" % item).encode('utf-8') #.strip()
     thefile.write(line_song)
+    lines.append(line_song)
 
-
-with open(file_name) as f:
-    lines = f.readlines()
+#with open(file_name) as f:
+#    lines = f.readlines()
+#    print lines
 
 dict = {}
 for str in lines:
-    key = str.rstrip('\n').lower()
-    if ((key!='') and (key!=' ') and key!='play video' and key!='encore:'):
-        if (key in dict):
-            dict[key]=dict[key]+1
-        else:
-            dict[key]=1
+	key = str.rstrip('\n').lower()
+	if ((key!='') and (key!=' ') and key!='play video' and key!='encore:'):
+		if (key in dict):
+			dict[key]=dict[key]+1
+		else:
+			dict[key]=1
 sorted = sorted(dict.items(), key=operator.itemgetter(1))
 
-rank_file_name = "%s_song_rank.txt"%artist_name
+rank_file_name = "%s_song_rank__%s_%s_%s.txt"%(artist_name, start_date, end_date, year)
 thefile_rank = open(rank_file_name, 'w')
 for tup in sorted:
     rank_line = "%s : %s"%(tup[0],tup[1])
